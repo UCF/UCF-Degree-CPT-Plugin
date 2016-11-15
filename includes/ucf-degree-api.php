9dd3819b1cc501cd7b160736e88367f49342f39a
Jim Barnes <james.barnes@ucf.edu>
2016-11-15 08:10:25 -0500

    Added comments and removed 'tags' field from the api response.

--- includes/ucf-degree-api.php
+++ includes/ucf-degree-api.php
@@ -6,10 +6,25 @@
  **/
 if ( ! class_exists( 'UCF_Degree_API' ) ) {
 	class UCF_Degree_API {
+
+		/**
+		 * Main entry point for Rest API functionality
+		 * @author Jim Barnes
+		 * @since 0.0.1
+		 * @return void
+		 **/
 		public static function register_rest_routes() {
 			add_action( 'rest_api_init', array( 'UCF_Degree_API', 'register_fields' ) );
+			add_action( 'rest_prepare_degree', array( 'UCF_Degree_API', 'remove_tags' ), 10, 3 );
 		}
 
+		/**
+		 * Registers the rest route by hooking into the custom post type args
+		 * @author Jim Barnes
+		 * @since 0.0.1
+		 * @param $args array | The arguments that will be passed to the register_post_type function.
+		 * @return array
+		 **/
 		public static function add_rest_route_to_args( $args ) {
 			$args['show_in_rest'] = true;
 			$args['rest_base'] = 'degrees';
@@ -18,6 +33,12 @@ if ( ! class_exists( 'UCF_Degree_API' ) ) {
 			return $args;
 		}
 
+		/**
+		 * Registers additional fields to the Rest API object
+		 * @author Jim Barnes
+		 * @since 0.0.1
+		 * @return void
+		 **/
 		public static function register_fields() {
 			$taxonomies = UCF_Degree_PostType::taxonomies();
 
@@ -33,9 +54,32 @@ if ( ! class_exists( 'UCF_Degree_API' ) ) {
 			}
 		}
 
-		public static function get_taxonomy_terms( $object, $tax_term, $request ) {
-			$terms = wp_get_post_terms( $object['id'], $tax_term );
+		/**
+		 * Gets all terms set to the post for the provided taxonomy
+		 * @author Jim Barnes
+		 * @since 0.0.1
+		 * @param $object array | The data object that will be returned to the Rest API
+		 * @param $tax string | The field name, which is this case should be a taxonomy slug
+		 * @param $request array | The request object, which includes get and post parameters
+		 * @return mixed | In this case a WP_Term object.
+		 **/
+		public static function get_taxonomy_terms( $object, $tax, $request ) {
+			$terms = wp_get_post_terms( $object['id'], $tax );
 			return $terms;
+		}
+
+		/**
+		 * Removes the 'tags' array, since it is included as 'post_tag'
+		 * @author Jim Barnes
+		 * @since 0.0.1
+		 * @param $data Object | The data object
+		 * @param $post WP_POST | The post object
+		 * @param $request Object | The request object
+		 * @return Object
+		 **/ 
+		public static function remove_tags( $data, $post, $request ) {
+			unset( $data->data['tags'] );
+			return $data;
 		}
 	}
 }
