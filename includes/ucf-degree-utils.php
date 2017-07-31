60e2a6a9d1cd40b379d950640e1ea816ed45669e
Jo Dickson <jogreybill@gmail.com>
2017-07-31 10:37:08 -0500

    Fixed calls to wpdb->prepare to suppress warnings

--- includes/ucf-degree-utils.php
+++ includes/ucf-degree-utils.php
@@ -119,9 +119,9 @@ if ( ! function_exists( 'ucf_degree_search_where_filter' ) ) {
 		if ( isset( $wp_query->query['degree_search'] ) && $wp_query->query_vars['post_type'] === 'degree' ) {
 			$s = $wp_query->query['degree_search'];
 			$where .= " AND (";
-			$where .= $wpdb::prepare( " lower($wpdb->posts.post_title) LIKE %s OR", '%' . $s . '%' );
-			$where .= $wpdb::prepare( " lower(wt.name) LIKE %s OR", '%' . $s . '%' );
-			$where .= $wpdb::prepare( " lower(wpm.meta_value) LIKE %s)", '%'. $s . '%' );
+			$where .= $wpdb->prepare( " lower($wpdb->posts.post_title) LIKE %s OR", '%' . $s . '%' );
+			$where .= $wpdb->prepare( " lower(wt.name) LIKE %s OR", '%' . $s . '%' );
+			$where .= $wpdb->prepare( " lower(wpm.meta_value) LIKE %s)", '%'. $s . '%' );
 		}
 
 		return $where;
