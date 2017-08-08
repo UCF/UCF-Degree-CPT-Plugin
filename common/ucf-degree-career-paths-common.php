a191c89b90f4d7dde49898a3503e1e497a6f2bdc
Jo Dickson <jogreybill@gmail.com>
2017-08-08 08:04:52 -0500

    Consolidated extra career path layout arguments into a single array 'args'; removed references to display_types since this plugin doesn't provide widgets

--- common/ucf-degree-career-paths-common.php
+++ common/ucf-degree-career-paths-common.php
@@ -4,34 +4,34 @@
  **/
 if ( ! class_exists( 'UCF_Degree_Career_Paths_Common' ) ) {
 	class UCF_Degree_Career_Paths_Common {
-		public function display_career_paths( $items, $layout, $title, $display_type='default' ) {
+		public function display_career_paths( $items, $layout, $args ) {
 			ob_start();
 
 			// Display before
-			$layout_before = ucf_career_paths_display_classic_before( '', $items, $title, $display_type );
+			$layout_before = ucf_career_paths_display_classic_before( '', $items, $args );
 			if ( has_filter( 'ucf_career_paths_display_' . $layout . '_before' ) ) {
-				$layout_before = apply_filters( 'ucf_career_paths_display_' . $layout . '_before', $layout_before, $items, $title, $display_type );
+				$layout_before = apply_filters( 'ucf_career_paths_display_' . $layout . '_before', $layout_before, $items, $args );
 			}
 			echo $layout_before;
 
 			// Display title
-			$layout_title = ucf_career_paths_display_classic_title( '', $items, $title, $display_type );
+			$layout_title = ucf_career_paths_display_classic_title( '', $items, $args );
 			if ( has_filter( 'ucf_career_paths_display_' . $layout . '_title' ) ) {
-				$layout_title = apply_filters( 'ucf_career_paths_display_' . $layout . '_title', $layout_title, $items, $title, $display_type );
+				$layout_title = apply_filters( 'ucf_career_paths_display_' . $layout . '_title', $layout_title, $items, $args );
 			}
 			echo $layout_title;
 
 			// Display items
-			$layout_content = ucf_career_paths_display_classic( '', $items, $title, $display_type );
+			$layout_content = ucf_career_paths_display_classic( '', $items, $args );
 			if ( has_filter( 'ucf_career_paths_display_' . $layout ) ) {
-				$layout_content = apply_filters( 'ucf_career_paths_display_' . $layout, $layout_content, $items, $title, $display_type );
+				$layout_content = apply_filters( 'ucf_career_paths_display_' . $layout, $layout_content, $items, $args );
 			}
 			echo $layout_content;
 
 			// Display after
-			$layout_after = ucf_career_paths_display_classic_after( '', $items, $title, $display_type );
+			$layout_after = ucf_career_paths_display_classic_after( '', $items, $args );
 			if ( has_filter( 'ucf_career_paths_display_' . $layout . '_after' ) ) {
-				$layout_after = apply_filters( 'ucf_career_paths_display_' . $layout . '_after', $layout_after, $items, $title, $display_type );
+				$layout_after = apply_filters( 'ucf_career_paths_display_' . $layout . '_after', $layout_after, $items, $args );
 			}
 			echo $layout_after;
 
@@ -41,7 +41,7 @@ if ( ! class_exists( 'UCF_Degree_Career_Paths_Common' ) ) {
 }
 
 if ( ! function_exists( 'ucf_career_paths_display_classic_before' ) ) {
-	function ucf_career_paths_display_classic_before( $content, $items, $title, $display_type ) {
+	function ucf_career_paths_display_classic_before( $content, $items, $args ) {
 		ob_start();
 	?>
 		<div class="ucf-career-paths-wrapper">
@@ -49,30 +49,25 @@ if ( ! function_exists( 'ucf_career_paths_display_classic_before' ) ) {
 		return ob_get_clean();
 	}
 
-	add_filter( 'ucf_career_paths_display_classic_before', 'ucf_career_paths_display_classic_before', 10, 4 );
+	add_filter( 'ucf_career_paths_display_classic_before', 'ucf_career_paths_display_classic_before', 10, 3 );
 }
 
 if ( ! function_exists( 'ucf_career_paths_display_classic_title' ) ) {
-	function ucf_career_paths_display_classic_title( $content, $items, $title, $display_type ) {
-		$formatted_title = $title;
+	function ucf_career_paths_display_classic_title( $content, $items, $args ) {
+		$formatted_title = '';
 
-		switch( $display_type ) {
-			case 'widget':
-				break;
-			case 'default':
-			default:
-				$formatted_title = '<h2 class="ucf-career-paths-title">' . $title . '</h2>';
-				break;
+		if ( $title = $args['title'] ) {
+			$formatted_title = '<h2 class=ucf-career-paths-title">' . $title . '</h2>';
 		}
 
 		return $formatted_title;
 	}
 
-	add_filter( 'ucf_career_paths_display_classic_title', 'ucf_career_paths_display_classic_title', 10, 4 );
+	add_filter( 'ucf_career_paths_display_classic_title', 'ucf_career_paths_display_classic_title', 10, 3 );
 }
 
 if ( ! function_exists( 'ucf_career_paths_display_classic' ) ) {
-	function ucf_career_paths_display_classic( $content, $items, $title, $display_type ) {
+	function ucf_career_paths_display_classic( $content, $items, $args ) {
 		ob_start();
 	?>
 		<ul class="ucf-career-paths-list">
@@ -84,11 +79,11 @@ if ( ! function_exists( 'ucf_career_paths_display_classic' ) ) {
 		return ob_get_clean();
 	}
 
-	add_filter( 'ucf_career_paths_display_classic', 'ucf_career_paths_display_classic', 10, 4 );
+	add_filter( 'ucf_career_paths_display_classic', 'ucf_career_paths_display_classic', 10, 3 );
 }
 
 if ( ! function_exists( 'ucf_career_paths_display_classic_after' ) ) {
-	function ucf_career_paths_display_classic_after( $content, $items, $title, $display_type ) {
+	function ucf_career_paths_display_classic_after( $content, $items, $args ) {
 		ob_start();
 	?>
 		</div>
@@ -96,7 +91,7 @@ if ( ! function_exists( 'ucf_career_paths_display_classic_after' ) ) {
 		return ob_get_clean();
 	}
 
-	add_filter( 'ucf_career_paths_display_classic_after', 'ucf_career_paths_display_classic_after', 10, 4 );
+	add_filter( 'ucf_career_paths_display_classic_after', 'ucf_career_paths_display_classic_after', 10, 3 );
 }
 
 ?>
