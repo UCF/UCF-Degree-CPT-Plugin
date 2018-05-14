d536d6270a060bd8389132c5fa22a288202d2190
Jim Barnes <james.barnes@ucf.edu>
2018-05-14 16:16:34 -0400

    Bumped version number:

--- ucf-degree-cpt.php
+++ ucf-degree-cpt.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Degree Custom Post Type
 Description: Provides a degree program custom post type, career paths and program type taxonomies and related meta fields.
-Version: 2.0.3
+Version: 3.0.0
 Author: UCF Web Communications
 License: GPL3
 */
